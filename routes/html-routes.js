33e9f5a4f953732865d7c5018f9ec2333e5402b8
jdvera <jdvera2015@gmail.com>
2018-01-05 13:23:30 -0600

    client-side js

---
+++ routes/html-routes.js
@@ -0,0 +1,24 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/location", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/location.html"));
+  });
+
+  app.get("/register", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/register.html"));
+  });
+
+  app.get("/results", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/results.html"));
+  });
+
+  app.get("/welcome", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/welcome.html"));
+  });
+};
